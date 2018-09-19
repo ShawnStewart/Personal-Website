3ae36f406a0e7eb00fd809d3c47377862b0e036f
Shawn Stewart <s.stewart0220@gmail.com>
2018-09-19 04:23:05 -0400

    Portfolio styling/animations

--- portfolio-site/src/Components/Portfolio/Portfolio.js
+++ portfolio-site/src/Components/Portfolio/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Reveal, Button } from "semantic-ui-react";
 import "./Portfolio.css";
 
 export default props => {
@@ -16,23 +16,49 @@ export default props => {
       </div>
       <Container>
         <div className="Portfolio__Body">
-          <div className="Portfolio__Project proj1">
-            <div>Project</div>
+          <div className="Portfolio__Project Project1">
+            <Reveal animated="fade" className="Project__Reveal">
+              <Reveal.Content visible className="Project__Reveal">
+                <div className="visible__content Project1__Visible">
+                  NextSteps
+                </div>
+              </Reveal.Content>
+              <Reveal.Content hidden className="Project__Reveal">
+                <div className="hidden__content Project1__Hidden">
+                  <h2>NextSteps</h2>
+                  <Button>Learn More</Button>
+                </div>
+              </Reveal.Content>
+            </Reveal>
           </div>
-          <div className="Portfolio__Project proj2">
-            <div>Project</div>
+
+          <div className="Portfolio__Project Project2">
+            <Reveal animated="move" className="Project__Reveal">
+              <Reveal.Content visible className="Project__Reveal">
+                <div className="visible__content Project2__Visible">
+                  Project 2
+                </div>
+              </Reveal.Content>
+              <Reveal.Content hidden className="Project__Reveal">
+                <div className="hidden__content Project2__Hidden">
+                  <h2>Project 2</h2>
+                  <Button>Learn More</Button>
+                </div>
+              </Reveal.Content>
+            </Reveal>
           </div>
+
           <div className="Portfolio__Project proj3">
-            <div>Project</div>
+            <div>Project 3</div>
           </div>
           <div className="Portfolio__Project proj4">
-            <div>Project</div>
+            <div>Project 4</div>
           </div>
           <div className="Portfolio__Project proj5">
-            <div>Project</div>
+            <div>Project 5</div>
           </div>
           <div className="Portfolio__Project proj6">
-            <div>Project</div>
+            <div>Project 6</div>
           </div>
         </div>
       </Container>
