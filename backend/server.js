70c08bab456fb57cddc2f2eedb552affecef4970
Shawn Stewart <s.stewart0220@gmail.com>
2018-09-20 20:19:09 -0400

    Setup backend server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 4000;
+
+// Serve static files from React App
+app.use(express.static(path.join(__dirname, "../leaderboard-frontend/build")));
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(port, () => console.log(`\nServer is runningon port ${port}`));
