221ea88f428e82adcc87a58d3c966a8a436c6122
Shawn Stewart <s.stewart0220@gmail.com>
2018-09-22 17:23:10 -0400

    Updated about me information and started responsive design

--- portfolio-site/src/Components/About/About.js
+++ portfolio-site/src/Components/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
-import selfie from "../../Img/selfie.jpg";
+import selfie from "../../Img/selfie2.jpg";
 import "./About.css";
 
 export default props => {
@@ -9,22 +9,27 @@ export default props => {
     <div className="AboutMe">
       <div className="About__Header">
         <Container textAlign="center">
-          <h2 className="Heading__Main">Who is Shawn?</h2>
-          <h3 className="Heading__Secondary">An ambitious young developer</h3>
+          <h2 className="Heading__Main">About Shawn</h2>
+          <h3 className="Heading__Secondary">
+            Developer of powerful user-friendly web applications
+          </h3>
         </Container>
       </div>
       <Container>
         <div className="About__Body">
-          <img src={selfie} alt="Selfie" className="Selfie" />
-          <p className="AboutMe__Content">
-            I am a full stack web developer. I am a full stack web developer. I
-            am a full stack web developer. I am a full stack web developer. I am
-            a full stack web developer. I am a full stack web developer. I am a
-            full stack web developer. I am a full stack web developer. I am a
-            full stack web developer. I am a full stack web developer. I am a
-            full stack web developer. I am a full stack web developer. I am a
-            full stack web developer. I am a full stack web developer.
-          </p>
+          <div className="Section1">
+            <img src={selfie} alt="Selfie" className="Selfie" />
+            <p className="AboutMe__Content">
+              I am a full stack web developer and graduate of{" "}
+              <a href="https://lambdaschool.com/" target="_blank">
+                Lambda School
+              </a>{" "}
+              as of September 2018. I much enjoy collaborating with talented
+              people to create highly impactful applications. I strive to
+              continuously expand my knowledge to increase my overall value as a
+              developer.
+            </p>
+          </div>
         </div>
       </Container>
     </div>
